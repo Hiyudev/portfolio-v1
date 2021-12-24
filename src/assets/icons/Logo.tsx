df2340f6e1714a8b104b1b99248666e96af78dbb
Keys HD <keys208@hotmail.com>
2021-12-24 17:21:26 -0300

    ✅ Add: Icons

---
+++ src/assets/icons/Logo.tsx
@@ -0,0 +1,20 @@
+const LogoIcon = () => {
+	return (
+		<svg
+			width="250"
+			height="216"
+			viewBox="0 0 250 216"
+			fill="currentColor"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M206.527 32.7895L187.5 -2.47384e-06L62.5 2.99009e-06L44.3996 31.1922L126.402 100.969L206.527 32.7895ZM238.299 87.5422L174.461 141.863L175.484 142.734L229.674 142.734L250 107.706L238.299 87.5422ZM88.0259 215.412L126.402 182.757L164.778 215.412L88.0259 215.412ZM78.3429 141.863L77.3196 142.734L20.3261 142.734L-4.70797e-06 107.706L12.6275 85.945L78.3429 141.863Z"
+				fill="currentColor"
+			/>
+		</svg>
+	);
+};
+
+export default LogoIcon;
---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,21 @@
+import CssIcon from './Css';
+import FirebaseIcon from './Firebase';
+import HtmlIcon from './Html';
+import JavascriptIcon from './Javascript';
+import NextIcon from './Next';
+import PythonIcon from './Python';
+import ReactIcon from './React';
+import TypescriptIcon from './Typescript';
+
+const icons = {
+	CssIcon,
+	FirebaseIcon,
+	HtmlIcon,
+	JavascriptIcon,
+	NextIcon,
+	PythonIcon,
+	ReactIcon,
+	TypescriptIcon,
+};
+
+export default icons;
