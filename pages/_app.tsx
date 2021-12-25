cdba68fbf38774dd60d3e16a165d355b5f1d5311
Keys HD <keys208@hotmail.com>
2021-12-25 10:48:43 -0300

    ✅ Add: Mega tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,48 @@ export default function App({ Component, pageProps }: AppProps) {
 		<Fragment>
 			<Head>
 				<title>Yuki&apos;s portfolio</title>
+				<meta name="title" content="Yuki's portfolio" />
+				<meta
+					name="description"
+					content="Hi ! I'm Yuki, a self-taught developer passionate about the technology world"
+				/>
+				<meta
+					name="keywords"
+					content="yuki2dev, keyshd, portfolio, github, programmer, developer"
+				/>
+				<link rel="icon" type="image/png" href="./favicon.ico" />
+
+				<meta name="theme-color" content="#09090D" />
+
+				<meta property="og:type" content="website" />
+				<meta
+					property="og:url"
+					content="https://portfolio-yuki2dev.vercel.app/"
+				/>
+				<meta property="og:title" content="Yuki's portfolio" />
+				<meta
+					property="og:description"
+					content="Hi ! I'm Yuki, a self-taught developer passionate about the technology world"
+				/>
+				<meta
+					property="og:image"
+					content="https://portfolio-yuki2dev.vercel.app/banner.png"
+				/>
+
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta
+					property="twitter:url"
+					content="https://portfolio-yuki2dev.vercel.app/"
+				/>
+				<meta property="twitter:title" content="Yuki's portfolio" />
+				<meta
+					property="twitter:description"
+					content="Hi ! I'm Yuki, a self-taught developer passionate about the technology world"
+				/>
+				<meta
+					property="twitter:image"
+					content="https://portfolio-yuki2dev.vercel.app/banner.png"
+				></meta>
 			</Head>
 			<ThemeWrapper>
 				<GlobalStyle />
