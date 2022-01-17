21719ffa4d027f8b3e6e67cbf425ef1b254ee2db
Yuki2dev <KeysHD@users.noreply.github.com>
2022-01-17 12:39:27 -0300

    ✅ Added: Meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,49 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider enableSystem attribute="class">
       <Head>
-        <title>Yuki's portfolio</title>
+        <title>Yuki&apos;s portfolio</title>
+        <meta name="title" content="Yuki's portfolio" />
+        <meta
+          name="description"
+          content="I'm Yuki, a self-taught developer passionate about the technology world"
+        />
+        <meta
+          name="keywords"
+          content="yuki2dev, keyshd, portfolio, github, programmer, developer"
+        />
+        <link rel="icon" type="image/png" href="./favicon.ico" />
+
+        <meta name="theme-color" content="#09090D" />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://portfolio-yuki2dev.vercel.app/"
+        />
+        <meta property="og:title" content="Yuki's portfolio" />
+        <meta
+          property="og:description"
+          content="Hi ! I'm Yuki, a self-taught developer passionate about the technology world"
+        />
+        <meta
+          property="og:image"
+          content="https://portfolio-yuki2dev.vercel.app/banner.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://portfolio-yuki2dev.vercel.app/"
+        />
+        <meta property="twitter:title" content="Yuki's portfolio" />
+        <meta
+          property="twitter:description"
+          content="Hi ! I'm Yuki, a self-taught developer passionate about the technology world"
+        />
+        <meta
+          property="twitter:image"
+          content="https://portfolio-yuki2dev.vercel.app/banner.png"
+        ></meta>
       </Head>
 
       <Component {...pageProps} />
